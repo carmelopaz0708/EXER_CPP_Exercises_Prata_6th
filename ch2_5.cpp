67877bc53ac002b83c728eeecaede854b267ca6f
Siege Paz <carmelopaz0708@gmail.com>
2019-03-16 05:40:21 +0800

    Update ch2_5.cpp

--- ch2_5.cpp
+++ ch2_5.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 
 double celsius_to_fahrenheit(double);
-void fahrenheit_display(double, double);
+void output_display(double, double);
 
 int main() {
   double input_celsius, output_fahrenheit;
   std::cout << "Please enter a Celsius value: ";
   std::cin >> input_celsius;
   output_fahrenheit = celsius_to_fahrenheit(input_celsius);
-  fahrenheit_display(input_celsius, output_fahrenheit);
+  output_display(input_celsius, output_fahrenheit);
 }
 
 double celsius_to_fahrenheit(double input_celsius) {
@@ -18,7 +18,7 @@ double celsius_to_fahrenheit(double input_celsius) {
   return output_fahrenheit;
 }
 
-void fahrenheit_display(double console_celsius, double console_fahrenheit) {
+void output_display(double console_celsius, double console_fahrenheit) {
   // Displays both degrees celsius and degrees fahrenheit
   std::cout << console_celsius << " degrees Celsius is " << console_fahrenheit << " degrees Fahrenheit";
 }
